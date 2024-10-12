0406aeee1b7a892ed8192b0a0299d8897451be2c
Johnny <qinjohnny1@gmail.com>
2024-10-12 16:21:48 +1100

    completed move_folder error cases

--- folder/move_folder.go
+++ folder/move_folder.go
@@ -1,7 +1,50 @@
 package folder
 
+import (
+	"errors"
+	"strings"
+
+	"github.com/gofrs/uuid"
+)
+
 func (f *driver) MoveFolder(name string, dst string) ([]Folder, error) {
-	// Your code here...
+
+	folders := f.folders
+
+	// Move folder to itself
+	if name == dst {
+		return []Folder{}, errors.New("Error: Cannot move a folder to itself")
+	}
+
+	var source_id uuid.UUID
+	var dest_id uuid.UUID
+	for _, f := range folders {
+		if strings.HasSuffix(f.Paths, name) { // Find source folder
+			source_id = f.OrgId
+		}
+		if strings.HasSuffix(f.Paths, dst) { // Find dest folder
+			dest_id = f.OrgId
+		}
+	}
+	// Source folder doesn't exist
+	if source_id.IsNil() {
+		return []Folder{}, errors.New("Error: Source folder does not exist")
+	}
+	// Destination folder doesn't exist
+	if dest_id.IsNil() {
+		return []Folder{}, errors.New("Error: Destination folder does not exist")
+	}
+
+	// Move folder to different organisation
+	if source_id != dest_id {
+		return []Folder{}, errors.New("Error: Cannot move a folder to a different organization")
+	}
+
+	// get all child folders
+
+	// find path of 'dst' from root
+
+	// append 'name'.{child_path} to 'path of dst from root'
 
 	return []Folder{}, nil
 }
